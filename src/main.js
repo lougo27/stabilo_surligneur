bcd1b2a10e9f4284ec94f70bf5f92a48d4e5ba4f
lougo27 <louanegouin@gmail.com>
2025-12-19 16:54:46 +0100

    fix: filtres cartes

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ class Home {
   constructor() {
     this.initSupabase();
     this.getDataCouleur();
+    this.filtrerParType();
     // this.getDataType();
     this.carteStabilo = "";
     this.imageStabilo = "";
@@ -15,114 +16,150 @@ class Home {
     this.hex = "";
     this.rgb = "";
 
-    this.divCartes = document.getElementById('conteneur_images');
+    this.allCards = [];
+
+    this.divCartes = document.getElementById("conteneur_images");
   }
 
   initSupabase() {
     this.superbase = createClient(
-      'https://ooiqcapsnvhygzllwxya.supabase.co',
-      'sb_publishable_UWU5Qu_447PWbMS-8wdqdQ_3IYaPbYl'
+      "https://ooiqcapsnvhygzllwxya.supabase.co",
+      "sb_publishable_UWU5Qu_447PWbMS-8wdqdQ_3IYaPbYl"
     );
-    console.log(this.superbase)
-
+    console.log(this.superbase);
   }
-  
+
   async getDataCouleur() {
     // Liée la ligne id_modele de la table couleur à la ligne id_modele de la table type
     const { data, error } = await this.superbase
-      .from('couleur')
-      .select('*, type(id_modele, modele)');
-      
+      .from("couleur")
+      .select("*, type(id_modele, modele)");
+
     console.log(data);
 
-    data.forEach(ligne => {
+    data.forEach((ligne) => {
       this.carte(ligne);
     });
   }
 
-  carte (ligne) {
+  carte(ligne) {
     //Creer la carte
-      this.carteStabilo = document.createElement('div');
-      this.carteStabilo.classList.add('carte');
+    this.carteStabilo = document.createElement("div");
+    this.carteStabilo.classList.add("carte");
 
-      //Creer l'image du stabilo
-      this.imageStabilo = document.createElement('img');
-      this.imageStabilo.src = ligne.image;
+    this.carteStabilo.setAttribute("data-id-modele", ligne.id_modele);
+    this.carteStabilo.setAttribute("data-id-couleur", ligne.id_couleur);
 
-      //Creer la div de la description
-      this.description = document.createElement('div');
-      this.description.classList.add('description');
-      // Nom du modèle
-      this.modele = document.createElement('h3');
-      this.modele.textContent = ligne.type.modele;
-      // Nom de la couleur
-      this.couleurNom = document.createElement('h3');
-      this.couleurNom.textContent = ligne.nom_couleur;
-      this.description.appendChild(this.modele);
-      this.description.appendChild(this.couleurNom);
+    //Creer l'image du stabilo
+    this.imageStabilo = document.createElement("img");
+    this.imageStabilo.src = ligne.image;
 
-      //Creer la div de la couleur
-      this.couleurDiv = document.createElement('div');
-      this.couleurDiv.classList.add('couleur');
-      // Box de la couleur
-      this.colorBox = document.createElement('div');
-      this.colorBox.classList.add('color-box');
-      this.colorBox.style.backgroundColor = ligne.code_hexadecimal;
-      // div Code de la couleur
-      this.couleurCode = document.createElement('div');
-      this.couleurCode.classList.add('couleur_code');
-      // Code hexadécimal
-      this.hex = document.createElement('p');
-      this.hex.textContent = ligne.code_hexadecimal;
-      // Code RVB
-      this.rgb = document.createElement('p');
-      this.rgb.textContent = "RVB " + ligne.code_RVB;
+    //Creer la div de la description
+    this.description = document.createElement("div");
+    this.description.classList.add("description");
+    // Nom du modèle
+    this.modele = document.createElement("h3");
+    this.modele.textContent = ligne.type.modele;
+    // Nom de la couleur
+    this.couleurNom = document.createElement("h3");
+    this.couleurNom.textContent = ligne.nom_couleur;
+    this.description.appendChild(this.modele);
+    this.description.appendChild(this.couleurNom);
 
-      // Bouton Edit
-      this.editButton = document.createElement('button');
-      this.editButton.classList.add('edit-button');
-      this.editIcon = document.createElement('img');
-      this.editIcon.src = '/public/images/crayon_rouge.png';
-      this.editIcon.alt = 'Edit';
-      this.editButton.appendChild(this.editIcon);
+    //Creer la div de la couleur
+    this.couleurDiv = document.createElement("div");
+    this.couleurDiv.classList.add("couleur");
+    // Box de la couleur
+    this.colorBox = document.createElement("div");
+    this.colorBox.classList.add("color-box");
+    this.colorBox.style.backgroundColor = ligne.code_hexadecimal;
+    // div Code de la couleur
+    this.couleurCode = document.createElement("div");
+    this.couleurCode.classList.add("couleur_code");
+    // Code hexadécimal
+    this.hex = document.createElement("p");
+    this.hex.textContent = ligne.code_hexadecimal;
+    // Code RVB
+    this.rgb = document.createElement("p");
+    this.rgb.textContent = "RVB " + ligne.code_RVB;
 
-      // Bouton croix 
-      this.deleteButton = document.createElement('button');
-      this.deleteButton.classList.add('delete-button');
-      this.deleteButton.textContent = 'X';
-      this.carteStabilo.appendChild(this.deleteButton);
+    // Bouton Edit
+    this.editButton = document.createElement("button");
+    this.editButton.classList.add("edit-button");
+    this.editIcon = document.createElement("img");
+    this.editIcon.src = "/public/images/crayon_rouge.png";
+    this.editIcon.alt = "Edit";
+    this.editButton.appendChild(this.editIcon);
 
-      //TOUS LES APPEND CHILD
-      this.couleurCode.appendChild(this.hex);
-      this.couleurCode.appendChild(this.rgb);
+    // Bouton croix
+    this.deleteButton = document.createElement("button");
+    this.deleteButton.classList.add("delete-button");
+    this.deleteButton.textContent = "X";
+    this.carteStabilo.appendChild(this.deleteButton);
 
-      this.couleurDiv.appendChild(this.colorBox);
-      this.couleurDiv.appendChild(this.couleurCode);
+    //TOUS LES APPEND CHILD
+    this.couleurCode.appendChild(this.hex);
+    this.couleurCode.appendChild(this.rgb);
 
-      this.carteStabilo.appendChild(this.imageStabilo);
-      this.carteStabilo.appendChild(this.description);
-      this.carteStabilo.appendChild(this.couleurDiv);
-      this.carteStabilo.appendChild(this.editButton);
+    this.couleurDiv.appendChild(this.colorBox);
+    this.couleurDiv.appendChild(this.couleurCode);
 
-      if (ligne.image != null) 
+    this.carteStabilo.appendChild(this.imageStabilo);
+    this.carteStabilo.appendChild(this.description);
+    this.carteStabilo.appendChild(this.couleurDiv);
+    this.carteStabilo.appendChild(this.editButton);
+
+    if (ligne.image != null) {
+      this.allCards.push(this.carteStabilo);
       this.divCartes.appendChild(this.carteStabilo);
+    }
+  }
 
-    } 
-  
+  filtrerParType() {
+    const boutonsFiltre = document.querySelectorAll(".bouton_filtre");
+    boutonsFiltre.forEach((bouton) => {
+      bouton.addEventListener("click", () => {
+        const modeleSelectionne = bouton.getAttribute("data-modele");
 
-  
+        this.allCards.forEach((carte) => {
+          const modelCurrentCard = carte.getAttribute("data-id-modele");
 
-  // async getDataType() {
-  //   const {data, error} = await this.superbase
-  //     .from('type')
-  //     .select();
+          carte.classList.remove("hidden");
+
+          if (modeleSelectionne === "all") {
+            return;
+          }
+
+          if (modelCurrentCard !== modeleSelectionne) {
+            carte.classList.add("hidden");
+          }
+        });
+
+        console.log(modeleSelectionne);
+      });
+    });
+  }
+
+  deleteCard () {
+    const boutonsDelete = document.querySelectorAll(".delete-button");
+    boutonsDelete.forEach((bouton) => {
+      bouton.addEventListener("click", () => {
+        this.deleteCard()
       
-  //   console.log(data);
-  // }
+
+      });
+
+
+    });
+  }
+
+    async deleteCard() {
+        const cardToDelete = bouton.getAttribute("data-id-couleur");
+        const supprimer = await supabase
+          .from('couleur')
+          .delete()
+          .eq('id_couleur', cardToDelete)
+        };
 }
 
-new Home()
-
-
-
-
+new Home();
